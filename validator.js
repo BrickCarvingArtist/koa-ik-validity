b1e6a0ad6c7f267e445b154756c9b2ff6d3ac7a4
pengcheng.yang <806321554@qq.com>
2017-11-09 22:58:58 +0800

    add jsdoc for the validator

--- validator.js
+++ validator.js
@@ -1,6 +1,12 @@
 const {isPlainObject, isUndefined, isNumber} = require("lodash");
 const table = require("./table");
 module.exports = {
+	/**
+	 * 数据值是否通过校验规则
+	 * @param {String} key 校验规则名
+	 * @param {String} value 数据值
+	 * @returns {Boolean} 检验是否通过
+	 */
 	_test(key, value, isPlain){
 		const expSetting = table[key];
 		let result;
@@ -18,6 +24,12 @@ module.exports = {
 		}
 		return result || expSetting.code;
 	},
+	/**
+	 * 校验各键所对应的值对是否通过校验规则
+	 * @public
+	 * @param {Object} parameterPairs 需要校验的键值对，键：校验规则名，值：数据值
+	 * @returns {Object} 各校验的结果
+	 */
 	test(parameterPairs){
 		let result = {};
 		if(!isPlainObject(parameterPairs)){
@@ -29,6 +41,12 @@ module.exports = {
 		}
 		return result;
 	},
+	/**
+	 * 校验各键所对应的值对是否通过校验规则
+	 * @public
+	 * @param {Object} parameterPairs 需要校验的键值对，键：校验规则名，值：数据值
+	 * @returns {Object} 各校验的结果。如果某条校验不通过，则返回错误码对象；如果通过，则返回空对象
+	 */
 	testForErrorCode(parameterPairs){
 		if(!isPlainObject(parameterPairs)){
 			throw table.keyValues;
